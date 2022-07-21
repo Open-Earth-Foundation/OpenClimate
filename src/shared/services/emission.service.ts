80de59f4651e605b0026c3c9a22ef6c7605ec2fa
cephaschapa <cephaschapa@gmail.com>
2022-07-21 15:20:36 +0200

    chore: Recoved console logs and shortened url

--- src/shared/services/emission.service.ts
+++ src/shared/services/emission.service.ts
@@ -1,11 +1,11 @@
 async function fetchEmissionsSubnational(subnational_id: number) {
-    let url = `https://dev.openclimate.network/api/subnationals/2019/${subnational_id}`;
+    let url = `/api/subnationals/2019/${subnational_id}`;
     var response = await fetch(url);
     return await response.json();
 }
 
 async function fetchEmissionsCity(city: number) {
-    let url = `https://dev.openclimate.network/api/city/2021/${city}`;
+    let url = `/api/city/2021/${city}`;
     var response = await fetch(url);
     return await response.json();
 }
