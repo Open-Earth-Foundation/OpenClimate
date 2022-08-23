346d6a6f8a05277cdd6882d74594d2f4e774400f
cephaschapa <cephaschapa@gmail.com>
2022-08-23 23:47:28 +0200

    Show climate action on account dashboard widget

--- src/components/account/subpages/account-dashboard/account-dashboard.tsx
+++ src/components/account/subpages/account-dashboard/account-dashboard.tsx
@@ -1,5 +1,8 @@
 import React, { FunctionComponent, useEffect, useState } from 'react'
 import IAggregatedEmission from '../../../../api/models/DTO/AggregatedEmission/IAggregatedEmission';
+import { ClimateActionScopes } from '../../../../api/models/DTO/ClimateAction/climate-action-scopes';
+import { ClimateActionTypes } from '../../../../api/models/DTO/ClimateAction/climate-action-types';
+import IClimateAction from '../../../../api/models/DTO/ClimateAction/IClimateAction';
 import ISite from '../../../../api/models/DTO/Site/ISite';
 import { IUser } from '../../../../api/models/User/IUser';
 import ClimateAccountabilityWidget from '../../../../shared/components/widgets/climate-accountability/climate-accountability.widget';
@@ -8,6 +11,7 @@ import PledgesWidget from '../../../../shared/components/widgets/pledges/pledges
 import ReportsWidget from '../../../../shared/components/widgets/reports/reports.widget';
 import SitesMapWidget from '../../../../shared/components/widgets/sites-map/sites-map.widget';
 import { AggregatedEmissionHelper } from '../../../../shared/helpers/aggregated-emission.helper';
+import { ClimateActionHelper } from '../../../../shared/helpers/climate-action.helper';
 import './account-dashboard.scss';
 
 
@@ -16,28 +20,58 @@ interface IProps  {
     aggregatedEmissions?: Array<IAggregatedEmission>,
     pledges?: Array<any>,
     sites?: Array<ISite>,
+    climateActions?: Array<IClimateAction>
     showModal: (entityType: string) => void
 }
 
 const AccountDashboard: FunctionComponent<IProps> = (props) => {
 
-    const { aggregatedEmissions, pledges, sites, user, showModal} = props;
+    const { aggregatedEmissions, pledges, sites,climateActions, user, showModal} = props;
 
     const [ summaryEmissions, setSummaryEmissions] = useState<IAggregatedEmission>();
-    console.log(aggregatedEmissions)
-
+    
     let widgetPledges = [];
     if(pledges)
         widgetPledges = pledges.slice(Math.max(pledges.length - 3, 0)).reverse();
     
 
     useEffect(() => {
-        if(aggregatedEmissions)
+        if(aggregatedEmissions?.length == 0)
         {
+            const scope1EmsActions = ClimateActionHelper.GetClimateActions(climateActions, ClimateActionScopes.Scope1 , ClimateActionTypes.Emissions);
+            const scope1EmsTotal = ClimateActionHelper.GetSumC02(scope1EmsActions, 'facility_emissions_co2e');
+
+            const scope2EmsActions = ClimateActionHelper.GetClimateActions(climateActions, ClimateActionScopes.Scope2 , ClimateActionTypes.Emissions);
+            const scope2EmsTotal = ClimateActionHelper.GetSumC02(scope2EmsActions, 'facility_emissions_co2e');
+
+            const scope3EmsActions = ClimateActionHelper.GetClimateActions(climateActions, ClimateActionScopes.Scope3 , ClimateActionTypes.Emissions);
+            const scope3EmsTotal = ClimateActionHelper.GetSumC02(scope3EmsActions, 'facility_emissions_co2e');
+
+            const scope1MtsActions = ClimateActionHelper.GetClimateActions(climateActions, ClimateActionScopes.Scope1 , ClimateActionTypes.Mitigations);
+            const scope1MtsTotal = ClimateActionHelper.GetSumC02(scope1MtsActions, 'facility_mitigations_co2e');
+
+            const scope2MtsActions = ClimateActionHelper.GetClimateActions(climateActions, ClimateActionScopes.Scope2 , ClimateActionTypes.Mitigations);
+            const scope2MtsTotal = ClimateActionHelper.GetSumC02(scope2MtsActions, 'facility_mitigations_co2e');
+
+            const scope3MtsActions = ClimateActionHelper.GetClimateActions(climateActions, ClimateActionScopes.Scope3 , ClimateActionTypes.Mitigations);
+            const scope3MtsTotal = ClimateActionHelper.GetSumC02(scope3MtsActions, 'facility_mitigations_co2e');
+
+            const totalScopeEmissions = scope1EmsTotal + scope2EmsTotal + scope3EmsTotal; 
+            const totalScopeMitigations = scope1MtsTotal + scope2MtsTotal + scope3MtsTotal;
+
+            const em: IAggregatedEmission = {
+                credential_category: "Climate Action",
+                facility_ghg_total_net_co2e: 500,
+                facility_ghg_total_gross_co2e:totalScopeEmissions,
+                facility_ghg_total_sinks_co2e:totalScopeMitigations,
+            }
+            setSummaryEmissions(em)
+        }
+        else{
             const summaryAggr = AggregatedEmissionHelper.GetSummaryAggregatedEmissions(aggregatedEmissions);
             setSummaryEmissions(summaryAggr);
         }
-    }, [aggregatedEmissions]);
+    }, [aggregatedEmissions, climateActions]);
 
     return (
         <div className="account-dasboard">
