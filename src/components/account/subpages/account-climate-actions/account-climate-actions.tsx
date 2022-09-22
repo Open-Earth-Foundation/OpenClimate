b57847d3d9bd9bc2b0952f2b82c476602e8a5138
cephaschapa <cephaschapa@gmail.com>
2022-09-22 16:39:01 +0200

    chore: hide toggle and grid view from climate action on account page

--- src/components/account/subpages/account-climate-actions/account-climate-actions.tsx
+++ src/components/account/subpages/account-climate-actions/account-climate-actions.tsx
@@ -1,5 +1,4 @@
 import React, { FunctionComponent, useState } from 'react'
-import Switcher from '../../../../shared/components/form-elements/switcher/switcher';
 import ClimateActionsGrid from './climate-actions-grid/climate-actions-grid';
 import ClimateActionsSummary from './climate-actions-summary/climate-actions-summary';
 import IClimateAction from '../../../../api/models/DTO/ClimateAction/IClimateActions/IClimateAction';
@@ -27,15 +26,6 @@ const ClimateActions: FunctionComponent<IProps> = (props) => {
 
     return (
         <div className="account__climate-actions">
-            <div className="account__climate-actions-switcher">
-                <Switcher
-                    leftOption="Summary view"
-                    rightOption="Grid view"
-                    className="climate-actions-switcher"
-                    leftOptionChosen={true}
-                    onChange={() => setGridView(!gridView)}
-                />
-            </div>
             {gridView ? 
                 <ClimateActionsGrid 
                     climateActions={climateActions}
