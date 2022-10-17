343af3105c12603b7b19077c542462ae6a4523b9
cephaschapa <cephaschapa@gmail.com>
2022-10-17 10:19:45 +0200

    feat: pledge component mockup added padding to review bottom reduced whitespaces

--- src/components/review/pledges-widget/pledges-widget.tsx
+++ src/components/review/pledges-widget/pledges-widget.tsx
@@ -1,9 +1,94 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './pledges-widget.scss';
+import {MdInfoOutline, MdArrowDownward} from "react-icons/md";
 
 const PledgesWidget = () => {
+    const [data, setData] = useState<boolean>(true)
     return(
-        <div>Pledges Widget</div>
+        <div className="pledges-widget" style={{height: data ? '': "268px"}}>
+            {
+                data ?
+                <div className="pledges-widget__wrapper">
+                    <div className="pledges-widget__metadata">
+                        <div>
+                            <div className='pledges-widget__metadata-inner'>
+                                <span className='pledges-widget__title'>
+                                    Pledges
+                                </span>
+                                <span>
+                                    <MdInfoOutline className="pledges-widget__icon"/>
+                                </span>
+                            </div>
+                            <span className="pledges-widget__last-updated">Last updated June 2020</span>
+                        </div>
+                    </div>
+                    <div className="pledges-widget__pledges-data">
+                        <div className="pledges-widget__pledge-entry">
+                            <div className="pledges-widget__pledge-source">
+                                <span>CDP</span>
+                            </div>
+                            <div className="pledges-widget__pledge-source-info">
+                                <div className="pledges-widget__pledge-type">
+                                    CARBON INTENSITY
+                                </div>
+                                <div className="pledges-widget__pledge-target">
+                                    <div className="pledges-widget__percentage-value">
+                                        <MdArrowDownward className="pledges-widget__percentage-arrow"/>
+                                        <span className="pledges-widget__percentage-value-number">30%</span>
+                                    </div>
+                                    <div className="pledges-widget__target-estimate">
+                                        <div>by 2030 relative</div> 
+                                        <div>to 2005</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="pledges-widget__pledge-entry">
+                            <div className="pledges-widget__pledge-source">
+                                <span>CDP</span>
+                            </div>
+                            <div className="pledges-widget__pledge-source-info">
+                                <div className="pledges-widget__pledge-type">
+                                    CARBON INTENSITY
+                                </div>
+                                <div className="pledges-widget__pledge-target">
+                                    <div className="pledges-widget__percentage-value">
+                                        <MdArrowDownward className="pledges-widget__percentage-arrow"/>
+                                        <span className="pledges-widget__percentage-value-number">50%</span>
+                                    </div>
+                                    <div className="pledges-widget__target-estimate">
+                                        <div>by 2025 relative</div> 
+                                        <div>to 2018</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="pledges-widget__pledge-entry">
+                            <div className="pledges-widget__pledge-source">
+                                <span>CDP</span>
+                            </div>
+                            <div className="pledges-widget__pledge-source-info">
+                                <div className="pledges-widget__pledge-type">
+                                    CARBON INTENSITY
+                                </div>
+                                <div className="pledges-widget__pledge-target">
+                                    <div className="pledges-widget__percentage-value">
+                                        <MdArrowDownward className="pledges-widget__percentage-arrow"/>
+                                        <span className="pledges-widget__percentage-value-number">0</span>
+                                    </div>
+                                    <div className="pledges-widget__target-estimate">
+                                        <div className='target-text'>by 2025</div> 
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>: 
+                <div>
+
+                </div>
+            }
+        </div>
     )
 }
 
--- src/components/review/review.page.tsx
+++ src/components/review/review.page.tsx
@@ -23,6 +23,7 @@ import {DonutChart} from 'react-circle-chart'
 import ITreaties from '../../api/models/DTO/Treaties/ITreaties';
 import IPledge from '../../api/models/DTO/Pledge/IPledge';
 import { ReviewHelper } from '../../shared/helpers/review.helper';
+import Bg from './img/Earth_Background_Home_Gray.png';
 
 
 
@@ -352,12 +353,12 @@ const ReviewPage: FunctionComponent<IProps> = (props) => {
         },       
     ]
     
-
+   
 
     return (
         <div className="review">
-            <div className="review__wrapper">
-                <div style={{backgroundColor : dashboardEntity ? "rgba(255,255,255, 0.8)": "", height: dashboardEntity ? "100vh": ""}} className="review__foreground">
+            <div className="review__wrapper" style={{backgroundImage: dashboardEntity ? `url(${Bg})`: ""}}>
+                <div style={{backgroundColor : dashboardEntity ? "rgba(255,255,255, 0.8)": "", height: dashboardEntity ? "100%": "", paddingBottom: "50px"}} className="review__foreground">
                     {
                         dashboardEntity ? '': 
                         <div className='review__background-content'>
@@ -401,32 +402,32 @@ const ReviewPage: FunctionComponent<IProps> = (props) => {
                                     <div className="review__earth-card-content">
                                         <div>
                                             <MdArrowUpward className="review__earth-card-item-icon"/>
-                                            <span className="review__earth-card-item-large-text">+49.8</span>
+                                            <span className="review__earth-card-item-large-text">49.8</span>
                                             <span className="review__earth-card-item-small-text">GtCO<sub>2</sub>eq</span>
                                         </div>
                                         <div className="review__earth-card-item-normal-text">in 2019</div>
                                     </div>
                                     <div className="review__earth-card-content donut-card">
-                                        <div>
-                                            <DonutChart items={items} size={50} showTotal={false} tooltipSx={{display: "none"}} trackColor="#D9D9D9"/>
+                                        <div className='donut'>
+                                            <DonutChart items={items} size={30} showTotal={false} tooltipSx={{display: "none"}} trackColor="#D9D9D9"/>
                                         </div>
                                         <div className='right-column'>
                                             <div>
                                                 <span className="review__earth-card-item-large-text">287.4</span>
                                                 <span className="review__earth-card-item-small-text">GtCO<sub>2</sub>eq</span>
                                             </div>
-                                            <div className="review__earth-card-item-normal-text">Left based on 1.5 target</div>
+                                            <div className="review__earth-card-item-normal-text target-text">Left based on 1.5 target</div>
                                         </div>
                                     </div>
                                     <div className="review__earth-card-content">
                                         <div>
                                             <MdArrowUpward className="review__earth-card-item-icon"/>
-                                            <span className="review__earth-card-item-large-text">+1.1 <sup>o</sup>C</span>
+                                            <span className="review__earth-card-item-large-text">1.1 <sup>o</sup>C</span>
                                             <span className="review__earth-card-item-small-text"></span>
                                         </div>
                                         <div className="review__earth-card-item-normal-text">Temperature <br /> since  1880</div>
                                     </div>
-                                    <div className="review__earth-card-content donut-card">
+                                    <div className="review__earth-card-content donut-card co2concentration">
                                         <div className=''>
                                             <div>
                                                 <MdArrowUpward className="review__earth-card-item-icon"/>
@@ -497,6 +498,7 @@ const ReviewPage: FunctionComponent<IProps> = (props) => {
                             <a href='mailto:climatedata@openearth.org'><button className="contact__button" >Contact us</button></a>
                         </div>
                     }
+                    
                 </div>
             </div>
         </div>
