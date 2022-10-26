581966cd5671d6e91f1998012a93d4ce0eb8f9e1
Evan Prodromou <evan@openearth.org>
2022-10-26 19:19:22 -0400

    fix(widget): trend precision
    Signed-off-by: Evan Prodromou <evan@openearth.org>

--- src/components/review/emissions-widget/emissions-widget.tsx
+++ src/components/review/emissions-widget/emissions-widget.tsx
@@ -26,7 +26,7 @@ const EmissionsWidget: FunctionComponent<Props> = (props) => {
     const years = (currentSource) ? current.emissions[currentSource].data.map((e:any) => e.year) : []
     const currentEmissions = (currentSource && currentYear) ? current.emissions[currentSource].data.find((e:any) => e.year == currentYear) : null
     const lastEmissions = (currentSource && currentYear) ? current.emissions[currentSource].data.find((e:any) => e.year == currentYear - 1) : null
-    const trend = (currentEmissions && lastEmissions) ? (currentEmissions.total_emissions - lastEmissions.total_emissions)/(lastEmissions.total_emissions).toPrecision(5) : 0
+    const trend = (currentEmissions && lastEmissions) ? (currentEmissions.total_emissions - lastEmissions.total_emissions)/(lastEmissions.total_emissions) : 0
 
     const yearChangeHandler = (e: SelectChangeEvent<number>) => {
         const value = e.target.value as number;
@@ -129,7 +129,7 @@ const EmissionsWidget: FunctionComponent<Props> = (props) => {
                                 trend != 0 &&
                                 <div className="emissions-widget__emissions-trend" >
                                     { trend > 0 ? <MdArrowDropUp className="emissions-widget__emissions-trend-icon-up"/> : <MdArrowDropDown className="emissions-widget__emissions-trend-icon-down"/>}
-                                    <span className={ trend > 0 ? "emissions-widget__emissions-trend-value-red" : "emissions-widget__emissions-trend-value-green"} >{ trend > 0 ? `+${trend}%` : `${trend}%` }</span>
+                                    <span className={ trend > 0 ? "emissions-widget__emissions-trend-value-red" : "emissions-widget__emissions-trend-value-green"} >{ `${(trend > 0) ? "+" : "-"}${trend.toPrecision(5)}%` }</span>
                                     <MdInfoOutline className="emissions-widget__icon trend-icon"/>
                                 </div>
                             }
