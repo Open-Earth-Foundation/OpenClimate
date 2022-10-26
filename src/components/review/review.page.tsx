be767126af0802452c9ead1b87a00c85e35bfedc
Evan Prodromou <evan@openearth.org>
2022-10-26 11:46:57 -0400

    chore(review): remove unused code and imports
    Signed-off-by: Evan Prodromou <evan@openearth.org>

--- src/components/review/review.page.tsx
+++ src/components/review/review.page.tsx
@@ -1,11 +1,9 @@
 import React, { FunctionComponent, useEffect } from 'react'
 import { DispatchThunk, RootState } from '../../store/root-state';
 import { connect } from 'react-redux';
-import ReviewInfo from './review-info/review-info';
 import ITrackedEntity from '../../api/models/review/entity/tracked-entity';
 import ReviewFilters from './review-filters/review-filters';
 import Dashboard from './review-dashboard/review-dashboard';
-import ContextBars from './review-context-bars/context-bars';
 import { DropdownOption } from '../../shared/interfaces/dropdown/dropdown-option';
 import {Oval} from "react-loader-spinner";
 import { FilterTypes } from '../../api/models/review/dashboard/filterTypes';
@@ -17,15 +15,12 @@ import * as appActions from '../../store/app/app.actions';
 import './review.page.scss';
 import '../explore/explore.page.scss'
 import {HiOutlineSearch} from 'react-icons/hi'
-import {MdClear, MdArrowUpward, MdArrowDropDown} from "react-icons/md";
+import { MdArrowUpward} from "react-icons/md";
 import {DonutChart} from 'react-circle-chart'
 
-import ITreaties from '../../api/models/DTO/Treaties/ITreaties';
 import IPledge from '../../api/models/DTO/Pledge/IPledge';
 import { ReviewHelper } from '../../shared/helpers/review.helper';
 import Bg from './img/Earth_Background_Home_Gray.png';
-
-
 
 interface IStateProps  {
     loading: boolean,
@@ -33,6 +28,7 @@ interface IStateProps  {
     selectedEntities: Array<ITrackedEntity>,
     reviewFilters: Array<IReviewFilter>
 }
+
 export interface IEmissionsData {
     sources: Array<string>,
     sourceToEmissions: Record<string, EmissionInfo>
@@ -52,13 +48,6 @@ export interface Emissions {
     methodologies: Array<string>;
 }
 
-interface IProviderData {
-    data_provider_id: number,
-    data_provider_name: string,
-    verified: boolean,
-    data_provider_link: string
-}
-
 interface IDispatchProps {
     selectFilter: (filterType: FilterTypes, option: DropdownOption, selectedEntities: Array<ITrackedEntity>) => void,
     deselectFilter: (filterType: FilterTypes) => void,
@@ -68,28 +57,10 @@ interface IDispatchProps {
 interface IProps extends IStateProps, IDispatchProps {
 }
 
-
-
 const ReviewPage: FunctionComponent<IProps> = (props) => {
-    const [selectNation, setSelectNation] = React.useState<boolean>(false);
     const [countryOptions, setCountryOptions] = React.useState<{}[]>([]);
-    const [nations, setNations] = React.useState<any>(countryOptions);
-    const [nationId, setNationId] = React.useState<number>();
-    const [snationId, setsNationId] = React.useState<number>();
-    const [stateValue, setStateV] = React.useState<string>();
-    const [providersData, setProviders] = React.useState<Array<string>>();
-    const [emissionsData, setEmissionsData] = React.useState<IEmissionsData>();
-    const [treatiesData, setTreatiesData] = React.useState<ITreaties>({});
     const [pledgesData, setPledgesData] = React.useState<Array<IPledge>>([]);
-    const [tgh, setTghg] = React.useState<number>();
     const [subns, setSbn] = React.useState<[]>();
-    const [subValue, setSubV] = React.useState<string>();
-    const [selectSub, setSelectSub] = React.useState<boolean>(false);
-    const [selectCity, setSelectCity] = React.useState<boolean>(false);
-    const [cityValue, setCityV] = React.useState<string>();
-    const [city, setCity] = React.useState<[]>();
-
-
 
     const { loading, dashboardEntityType, selectedEntities, reviewFilters, selectFilter, deselectFilter, showModal  } = props;
     let dashboardEntity:ITrackedEntity|null = null;
@@ -106,62 +77,18 @@ const ReviewPage: FunctionComponent<IProps> = (props) => {
     }
     const error = '';
 
-    const handleDropNation = () => {
-        setSelectNation((p)=>!p);
-        if(selectSub){
-            setSelectSub(false)
-        }
-        if(selectCity){
-            setSelectCity(false)
-        }
-    }
-
     const getAllcountries = async () => {
         const countries = await  fetch('/api/country', {
 
             method: 'GET',
         });
         const jsonData = await countries.json()
-        // console.log(jsonData.data)
         setCountryOptions(jsonData.data)
     }
 
     React.useEffect(()=> {
         getAllcountries()
     },[])
-
-    interface INation {
-        country_id: number;
-        country_name: string;
-    }
-
-    interface ISubNation {
-        subnational_id: number,
-        subnational_name: string
-    }
-
-    interface ICity {
-        city_id: string,
-        city_name: string
-    }
-
-    const handleFilter = (e:any) => {
-        const val = e.target.value.toLocaleLowerCase();
-        const country = countryOptions.filter(v => {
-            return Object.values(v).join('').toLocaleLowerCase().includes(val)
-        });
-        setNations(country)
-    }
-
-    // useEffect(()=> {
-
-    //     fetchProviderData();
-    // },[])
-
-    const handleTreaties = async (countryCode: string) => {
-        const treaties = await ReviewHelper.LoadTreatiesCountry(countryCode);
-        setTreatiesData(treaties);
-    }
 
     const addPledgesData = (newPledges: Array<IPledge>) => setPledgesData(pledgesData.concat(newPledges));
 
@@ -181,96 +108,6 @@ const ReviewPage: FunctionComponent<IProps> = (props) => {
         }
     }
 
-    const createProviderEmissionsData = (data: Array<any>) => {
-        const providerToEmissions: Record<string, EmissionInfo> = {};
-        data.forEach(emission => {
-            const dataProviderName = emission.DataProvider?.data_provider_name;
-
-            if (dataProviderName && !providerToEmissions[dataProviderName]) {
-                const methodologyTags = emission.DataProvider.Methodology.Tags.map((tag:any) => tag.tag_name);
-                const emissionData: EmissionInfo = {
-                    actorType: emission.actor_type,
-                    totalGhg: emission.total_ghg_co2e,
-                    year: emission.year,
-                    landSinks: emission.land_sinks,
-                    otherGases: emission.other_gases,
-                    methodologyType: emission.DataProvider?.Methodology?.methodology_type ?? '',
-                    methodologyTags: methodologyTags
-                }
-                providerToEmissions[dataProviderName] = emissionData;
-            }
-            else if (providerToEmissions[dataProviderName].year === emission.year) {
-                if (!providerToEmissions[dataProviderName].totalGhg && emission.total_ghg_co2e) {
-                    providerToEmissions[dataProviderName].totalGhg = emission.total_ghg_co2e;
-                }
-                if (!providerToEmissions[dataProviderName].landSinks && emission.land_sinks) {
-                    providerToEmissions[dataProviderName].landSinks = emission.land_sinks;
-                }
-            }
-        })
-
-        return providerToEmissions;
-    }
-
-
-    const setStateValue = async (e:any) =>{
-        e.preventDefault();
-        const nationalId:number = e.target.getAttribute("data-id");
-        setStateV(e.target.value)
-        console.log(e.target.value)
-        setNationId(nationalId)
-        setSelectNation((p)=>!p);
-        console.log(nationId)
-
-        // fetch data
-        fetchData(nationalId);
-        setSubV('');
-        setCityV('')
-
-    }
-
-    const fetchData = async (id:any) => {
-
-        const fetchCountryData = await fetch(`/api/country/2019/${id}`);
-
-        const jsonData = await fetchCountryData.json();
-        console.log('JSON DATA', jsonData);
-        setTghg(jsonData.data[0].Emissions[0].total_ghg_co2e)
-        const providerToEmissionsData = createProviderEmissionsData(jsonData.data[0].Emissions)
-
-        const data = {
-            actor_name: jsonData.data[0].country_name,
-            flag_icon: jsonData.data[0].flag_icon,
-            providerToEmissions: providerToEmissionsData
-        }
-
-        const countryCode = jsonData.data[0].iso
-        handleTreaties(countryCode);
-        handlePledges(countryCode, 'country');
-        setEmissionsData(data);
-
-        setSbn(jsonData.data[0].Subnationals)
-    }
-
-    const fetchSubnationalData = async (id:any) => {
-        const fetchCountryData = await fetch(`/api/subnationals/2019/${id}`);
-        const jsonData = await fetchCountryData.json();
-        console.log(jsonData);
-        const providerToEmissionsData = createProviderEmissionsData(jsonData.data[0].Emissions)
-        setTghg(jsonData.data[0].Emissions[0].total_ghg_co2e)
-
-        const data = {
-            actor_name: jsonData.data[0].subnational_name,
-            flag_icon: jsonData.data[0].flag_icon,
-            providerToEmissions: providerToEmissionsData
-        }
-
-        setEmissionsData(data);
-        handlePledges(data.actor_name, 'subnational');
-
-        setCity(jsonData.data[0].Cities);
-    }
-
     useEffect(()=> {
         if(subns) {
             console.log(subns);
@@ -285,66 +122,6 @@ const ReviewPage: FunctionComponent<IProps> = (props) => {
         }
     }, [dashboardEntity]);
 
-    const setSubnationValue = (e:any) =>{
-        const sbnlId:number = e.target.getAttribute("data-id");
-        setSubV(e.target.value)
-        setsNationId(sbnlId)
-        e.preventDefault();
-        setSubV(e.target.value)
-        setSelectSub((p)=>!p);
-        console.log(sbnlId)
-        fetchSubnationalData(sbnlId)
-        setCityV('')
-    }
-
-    const fetchCityData = async (id:any) => {
-        const fetchCountryData = await fetch(`/api/city/2019/${id}`);
-        const jsonData = await fetchCountryData.json();
-
-        setTghg(jsonData.data[0].Emissions[0].total_ghg_co2e);
-        const providerToEmissionsData = createProviderEmissionsData(jsonData.data[0].Emissions)
-
-        const data = {
-            actor_name: jsonData.data[0].country_name,
-            flag_icon: jsonData.data[0].flag_icon,
-            providerToEmissions: providerToEmissionsData
-        }
-        setEmissionsData(data);
-        setSbn(jsonData.data[0].Subnationals);
-    }
-
-    const setCityValue = (e:any) =>{
-        const cityId:number = e.target.getAttribute("data-id");
-        // setSubV(e.target.value)
-        setsNationId(cityId)
-        e.preventDefault();
-        setCityV(e.target.value)
-        setSelectCity((p)=>!p);
-        console.log(cityId)
-        fetchCityData(cityId)
-    }
-    const handleSub = () => {
-        setSelectSub((p)=>!p);
-        if(selectCity){
-            setSelectCity(false)
-        }
-        if(selectNation){
-            setSelectNation(false)
-        }
-    }
-
-    const handleDropCity = () => {
-        setSelectCity((p)=>!p);
-        if(selectNation){
-            setSelectNation(false)
-        }
-        if(selectSub){
-            setSelectSub(false)
-        }
-    }
-
-    console.log(dashboardEntity)
-
     // Donut earth props items
     const items = [
         {
@@ -356,17 +133,15 @@ const ReviewPage: FunctionComponent<IProps> = (props) => {
             value: 68,
             label: "Difference",
             color: "#FA7200"
-        },       
+        },
     ]
-    
-   
 
     return (
         <div className="review">
             <div className="review__wrapper" style={{backgroundImage: dashboardEntity ? `url(${Bg})`: ""}}>
                 <div style={{backgroundColor : dashboardEntity ? "rgba(255,255,255, 0.8)": "", height: dashboardEntity ? "100%": "", paddingBottom: "50px"}} className="review__foreground">
                     {
-                        dashboardEntity ? '': 
+                        dashboardEntity ? '':
                         <div className='review__background-content'>
                             <div className='review__background-content-left'></div>
                         </div>
@@ -402,7 +177,7 @@ const ReviewPage: FunctionComponent<IProps> = (props) => {
                             <span className="review__actor-type">Global</span>
                             <div className="review__earth-card">
                                 <div className="review__earth-card-head">
-                                    <span>Earth</span> 
+                                    <span>Earth</span>
                                 </div>
                                 <div className='review__earth-card-body'>
                                     <div className="review__earth-card-content">
@@ -443,11 +218,11 @@ const ReviewPage: FunctionComponent<IProps> = (props) => {
                                             <div className="review__earth-card-item-normal-text">atmospheric CO<sub>2 </sub>concentration</div>
                                         </div>
                                     </div>
-                                    
+
                                 </div>
                             </div>
                         </div>
-                        
+
                         <div className="review__filter-button-wrapper">
                             <a href='/explore'>
                                 <button className="review__filter-button">
@@ -495,7 +270,6 @@ const ReviewPage: FunctionComponent<IProps> = (props) => {
                         }
                     </div>
 
-
                     {
                         dashboardEntity ? "" :
                         <div className="contact__block">
@@ -504,13 +278,12 @@ const ReviewPage: FunctionComponent<IProps> = (props) => {
                             <a href='mailto:climatedata@openearth.org'><button className="contact__button" >Contact us</button></a>
                         </div>
                     }
-                    
+
                 </div>
             </div>
         </div>
     );
 }
-
 
 const mapStateToProps = (state: RootState, ownProps: any) => {
 
