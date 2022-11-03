3c8462ba2d622dc1b4394b4c2c2c13ebabbdda05
cephaschapa <cephaschapa@gmail.com>
2022-11-03 17:14:40 +0200

    fix: hint card title and body text

--- src/components/review/CollaborateCardHint.tsx
+++ src/components/review/CollaborateCardHint.tsx
@@ -24,11 +24,11 @@ const CollaborationCardHint = () => {
                 hasReadHint ? 
                 <div className="review-collaboration-card-hint">
                     <div className="collaboration-card-header">
-                        <span>Want to <br /> contribute?</span>
+                        <span>Want to <br /> contribute with data?</span>
                         <CloseIcon onClick={setCollabCookieValue} className="collaboration-card-close-icon"/>
                     </div>
                     <div className="collaboration-card-body">
-                        <p>Find out how to be <br /> apart of the <br /> <span>independent stocktaking effort</span> <br /> by collaborating <br /> with us.</p>
+                        <p>Find out how to be <br /> a part of the <br /> <span>independent stocktaking effort</span> <br /> by collaborating <br /> with us.</p>
                     </div>
                     <div className="collaboration-card-footer">
                         <ArrowDownward className="collaboration-card-arrow-icon"/>
