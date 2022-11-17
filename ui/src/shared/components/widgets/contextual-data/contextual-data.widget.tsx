cfe7a0ec80f4c3a8741e58b0a23a652490f6fad9
root <root@DESKTOP-RGGI50S>
2022-11-17 14:55:43 +0200

    feat: added collaborate cta and message to empty state widgets

--- ui/src/shared/components/widgets/contextual-data/contextual-data.widget.tsx
+++ ui/src/shared/components/widgets/contextual-data/contextual-data.widget.tsx
@@ -6,6 +6,7 @@ import { DonutChart } from 'react-circle-chart';
 import { Boy, AspectRatio, InfoOutlined, MonetizationOnOutlined } from '@mui/icons-material'
 import { makeStyles } from "@material-ui/core/styles";
 import Tooltip from '@mui/material/Tooltip';
+import Diversity3Icon from '@mui/icons-material/Diversity3';
 
 interface Props {
     current: any,
@@ -62,10 +63,13 @@ const ContextualDataWidget: FunctionComponent<Props> = (props) => {
             label: 'N/A',
             color: '#D9D9D9'
         }]
+    console.log(current);
 
     return (
         <div className="contextual-widget" >
-            <div className="contextual-widget__wrapper" >
+            {
+                current.gdp && current.population && current.area ? (
+                    <div className="contextual-widget__wrapper" >
                 <div className="contextual-widget__header">
                     <div className="contextual-widget__title-wrapper">
                         <h3 className="contextual-widget__title">
@@ -196,6 +200,39 @@ const ContextualDataWidget: FunctionComponent<Props> = (props) => {
                 </div>
 
             </div>
+                ):(
+                    <>
+                        <div className="contextual-widget__wrapper" >
+                            <div className="contextual-widget__header">
+                                <div className="contextual-widget__title-wrapper">
+                                    <h3 className="contextual-widget__title">
+                                        Contextual Data
+                                    </h3>
+                                </div>
+
+                                {   lastUpdatedYear &&
+                                        <span className="contextual-widget__updated">Last updated in {lastUpdatedYear}</span>
+                                }
+
+                            </div>
+                            <div className="contextual-widget__data">
+                                <div className="contextual-widget__contextual-empty-state">
+                                    
+                                    <p>There's no data available, if you have any suggested <br /> data sources or you are a provider please</p>
+                                    
+                                    <button className="collaborate-cta-btn">
+                                        <Diversity3Icon className="collaborate-cta-icon"/>
+                                        <span>COLLABORATE WITH DATA</span>
+                                    </button>
+
+                                </div>
+                            </div>
+                        </div>
+                    </>
+                )
+            
+            }
+            
         </div>
 
     );
