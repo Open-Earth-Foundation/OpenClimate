f6cb1a5df19a47cc78b5765b0cb64b43fda6340b
Gaurab Pant <gaurab@openearth.org>
2022-12-09 19:09:24 -0500

    Squashed 'ui/' changes from b9d8155..f389a99
    f389a99 fix(matomo): hack around the problems with the Matomo code
    4871a90 chore(version): 1.4.0-dev
    a03426b chore(version): 1.3.0-rc0
    f1d8086 fix(OC-1266): overflowing pledges
    0ea4a42 fix(OC-1147): collaborate with data link
    d0e973f fix(OC-1291): reduce size of Earth background
    
    git-subtree-dir: ui
    git-subtree-split: f389a993d80ffb69e4096c53bedcc907b3a121e5

--- src/components/review/pledges-widget/pledges-widget.tsx
+++ src/components/review/pledges-widget/pledges-widget.tsx
@@ -39,7 +39,7 @@ const PledgesWidget: FunctionComponent<Props> = (props) => {
     const lastUpdated = (lu.length > 0) ? lu[lu.length - 1] : null
     const [lastMonth, lastYear] = (lastUpdated) ? [(new Date(lastUpdated)).getMonth(), (new Date(lastUpdated)).getFullYear()] : [null, null]
 
-    
+
     return(
         <div className="pledges-widget" style={{height: targets.length ? '': "268px"}}>
             {
@@ -52,7 +52,7 @@ const PledgesWidget: FunctionComponent<Props> = (props) => {
                                     Pledges
                                 </span>
                                 <span>
-                                <Tooltip 
+                                <Tooltip
                                     classes={{
                                                 tooltip: classes.customTooltip,
                                                 arrow: classes.customArrow
@@ -61,11 +61,11 @@ const PledgesWidget: FunctionComponent<Props> = (props) => {
                                                 <div className = "tooltip">
                                                     Commitments that the selected actor made
                                                 </div>
-                                           } 
+                                           }
                                     arrow placement="right">
                                         <InfoOutlined className="pledges-widget__icon info-icon" />
                                 </Tooltip>
-                                    
+
                                 </span>
                             </div>
                             {
@@ -74,9 +74,11 @@ const PledgesWidget: FunctionComponent<Props> = (props) => {
                             }
                         </div>
                     </div>
-                    { targets?.map((pledge:any, index: number) =>
-                        <PledgeItem pledge={pledge} key={`pledge-item-${index}`} />
-                    )}
+                    <div className='pledges-widget__pledge-items'>
+                        { targets?.map((pledge:any, index: number) =>
+                            <PledgeItem pledge={pledge} key={`pledge-item-${index}`} />
+                        )}
+                    </div>
                 </div>:
                 <div className="pledges-widget__wrapper">
                     <div className="pledges-widget__metadata">
@@ -86,7 +88,7 @@ const PledgesWidget: FunctionComponent<Props> = (props) => {
                                     Pledges
                                 </span>
                                 <span>
-                                <Tooltip 
+                                <Tooltip
                                     classes={{
                                                 tooltip: classes.customTooltip,
                                                 arrow: classes.customArrow
@@ -95,7 +97,7 @@ const PledgesWidget: FunctionComponent<Props> = (props) => {
                                                 <div className = "tooltip">
                                                     Commitments that the selected actor made
                                                 </div>
-                                           } 
+                                           }
                                     arrow placement="right">
                                         <InfoOutlined className="pledges-widget__icon info-icon" />
                                 </Tooltip>
@@ -106,12 +108,12 @@ const PledgesWidget: FunctionComponent<Props> = (props) => {
                     </div>
                     <div className="pledges-widget__pledges-data no-data">
                         <div className="pledges-widget__pledges-empty-state">
-                            
+
                             <p>There's no data available, if you have any suggested <br /> data sources or you are a provider please</p>
-                            
+
                             <button className="collaborate-cta-btn">
                                 <Diversity3Icon className="collaborate-cta-icon"/>
-                                <span>COLLABORATE WITH DATA</span>
+                                <a href="https://docs.google.com/forms/d/e/1FAIpQLSfL2_FpZZr_SfT0eFs_v4T5BsZnrNBbQ4pkbZ51JhJBCcud6A/viewform?pli=1&pli=1">COLLABORATE WITH DATA</a>
                             </button>
 
                         </div>
