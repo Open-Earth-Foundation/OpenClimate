fcd17bcedc77bc7e497e9881d44434aa935021db
Evan Prodromou <evan@openearth.org>
2023-01-28 13:39:50 -0500

    feat(import): add logging to import_all_data.py
    Signed-off-by: Evan Prodromou <evan@openearth.org>

--- import/import_all_data.py
+++ import/import_all_data.py
@@ -1,9 +1,14 @@
 # import all files
 
+import logging
 from pathlib import Path
 from import_openclimate_data import import_openclimate_data
 
 def import_all_data(processed, host, dbname, user, password):
+
+    logger = logging.getLogger(__name__)
+
+    logger.info('Beginning import')
 
     prereqs = [
         "ISO-3166-1",
@@ -13,17 +18,25 @@ def import_all_data(processed, host, dbname, user, password):
     ]
 
     for prereq in prereqs:
+        logger.info(f'Importing {prereq}')
         import_openclimate_data(processed + '/' + prereq, host, dbname, user, password)
 
     pp = Path(processed)
 
+    logger.info('Beginning import')
+
     for f in pp.iterdir():
         if f.is_dir() and f.name not in prereqs:
-            import_openclimate_data(str(f), host, dbname, user, password)
+            logger.info(f'Importing {f.name}')
+            try:
+                import_openclimate_data(str(f), host, dbname, user, password)
+            except Exception as err:
+                logger.error('Error importing %s: %s', f.name, err)
 
 if __name__ == "__main__":
     import argparse
     import os
+    logging.basicConfig(level=logging.INFO)
     parser = argparse.ArgumentParser()
     parser.add_argument('--dbname', help='database name', default=os.environ.get('OPENCLIMATE_DATABASE'))
     parser.add_argument('--user', help='database user', default=os.environ.get('OPENCLIMATE_USER'))
