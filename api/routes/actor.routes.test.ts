af48ef4076e07afb9cf15d3b743bcf4716729fe8
gaurab-pant <gaurab@openearth.org>
2023-02-17 12:04:39 -0500

    feat(separated emission routes): added new download route and updates to tests
    Signed-off-by: gaurab-pant <gaurab@openearth.org>

--- api/routes/actor.routes.test.ts
+++ api/routes/actor.routes.test.ts
@@ -947,12 +947,11 @@ it("can get path to the actor", async () =>
       expect(data[4].actor_id).toEqual("EARTH");
     }));
 
-it("can get actor emissions in json", async () =>
+it("can get actor emissions", async () =>
   request(app)
     .get(`/api/v1/actor/${country1Props.actor_id}/emissions`)
     .expect(200)
     .expect("Content-Type", /json/)
-    .expect("Content-Disposition", /attachment; filename=/)
     .expect((res: any) => {
       const emissions = res.body.data;
 
@@ -998,3 +997,55 @@ it("can get actor emissions in json", async () =>
 
 it("gets 404 when actor id is nonexistent for emissions", async () =>
   request(app).get(`/api/v1/actor/${ACTOR_DNE}/emissions`).expect(404));
+
+it("can download actor emissions in json", async () =>
+  request(app)
+    .get(`/api/v1/download/${country1Props.actor_id}-emissions.json`)
+    .expect(200)
+    .expect("Content-Type", /json/)
+    .expect("Content-Disposition", /attachment/)
+    .expect((res: any) => {
+      const emissions = res.body;
+
+      console.log(res.body);
+
+      const emissions1 = emissions[datasource1Props.datasource_id];
+
+      expect(emissions1.tags).toBeDefined();
+      expect(emissions1.tags.length).toEqual(4);
+      expect(emissions1.tags[0].tag_id).toBeDefined();
+      expect(emissions1.tags[0].tag_name).toBeDefined();
+      expect(emissions1.data.length).toEqual(20);
+
+      const ea1992 = emissions1.data.find((ea) => ea.year == 1992);
+
+      expect(ea1992).toBeDefined();
+      expect(ea1992.tags).toBeDefined();
+      expect(ea1992.tags.length).toEqual(1);
+      expect(ea1992.tags[0].tag_id).toBeDefined();
+      expect(ea1992.tags[0].tag_name).toBeDefined();
+
+      expect(emissions[datasource2Props.datasource_id]).toBeDefined();
+
+      const emissions2 = emissions[datasource2Props.datasource_id];
+
+      expect(emissions2).toBeDefined();
+      expect(emissions2.tags).toBeDefined();
+      expect(emissions2.tags.length).toEqual(2);
+      expect(emissions2.tags[0].tag_id).toBeDefined();
+      expect(emissions2.tags[0].tag_name).toBeDefined();
+
+      expect(emissions2.data).toBeDefined();
+      expect(emissions2.data.length).toEqual(20);
+
+      const ea2002 = emissions2.data.find((ea) => ea.year == 2002);
+
+      expect(ea2002).toBeDefined();
+      expect(ea2002.tags).toBeDefined();
+      expect(ea2002.tags.length).toEqual(1);
+      expect(ea2002.tags[0].tag_id).toBeDefined();
+      expect(ea2002.tags[0].tag_name).toBeDefined();
+    }));
+
+it("gets 404 when actor id is nonexistent for downloading emissions in json", async () =>
+  request(app).get(`/api/v1/download/${ACTOR_DNE}-emissions.json`).expect(404));
