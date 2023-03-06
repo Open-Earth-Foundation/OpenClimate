46507c8c82f76a9491899c11d7ffa52c4cb2c010
Evan Prodromou <evan@openearth.org>
2023-03-06 11:34:39 -0500

    feat(schema): update DataSourceQuality table
    Signed-off-by: Evan Prodromou <evan@openearth.org>

--- api/schema/SQL/DataSourceQuality.sql
+++ api/schema/SQL/DataSourceQuality.sql
@@ -2,14 +2,15 @@
  * such as calculating percent achieved
  */
 
-CREATE TABLE "DataSourceApproved" (
+CREATE TABLE "DataSourceQuality" (
   "datasource_id" varchar(255), /* Which data source */
-  "ghg_target_percent_achieved" REAL, /* quality score between 0 - 1 */
+  "score_type" varchar(255), /* probably */
+  "score" REAL, /* quality score between 0 - 1 */
   "notes" TEXT, /* why score chosen for each field */
   "created" timestamp,
   "last_updated" timestamp,
-  PRIMARY KEY ("datasource_id"),
-  CONSTRAINT "FK_DataSourceApproved.datasource_id"
+  PRIMARY KEY ("datasource_id", "score_type"),
+  CONSTRAINT "datasourcequality_datasource_datasource_id"
     FOREIGN KEY ("datasource_id")
       REFERENCES "DataSource"("datasource_id"),
 );
