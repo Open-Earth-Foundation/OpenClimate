26e9ea69176029f25e90c074db5a39738a0bb4ac
Evan Prodromou <evan@openearth.org>
2023-03-10 17:25:41 -0500

    feat(logging): add OPENCLIMATE_LOG_LEVEL to api
    Signed-off-by: Evan Prodromou <evan@openearth.org>

--- api/logger.ts
+++ api/logger.ts
@@ -1,16 +1,17 @@
 const winston = require("winston");
 const { format } = winston;
 
-// We have a single app-wide root logger
+// Use the environment variable for the log level
+// or pick an appropriate level based on NODE_ENV
 
 let level =
-  process.env.NODE_ENV === "production"
-    ? "info"
-    : process.env.NODE_ENV === "development"
-    ? "debug"
-    : process.env.NODE_ENV === "test"
-    ? "crit"
-    : "error";
+  (process.env.OPENCLIMATE_LOG_LEVEL) ? process.env.OPENCLIMATE_LOG_LEVEL :
+  (process.env.NODE_ENV === "production") ? "info" :
+  (process.env.NODE_ENV === "development") ? "debug" :
+  (process.env.NODE_ENV === "test") ? "crit" :
+  "error";
+
+// We have a single app-wide root logger
 
 module.exports = winston.createLogger({
   level: level,
