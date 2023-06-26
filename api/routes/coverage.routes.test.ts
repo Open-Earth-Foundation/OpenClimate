58f74c67df318eb57b1beeeea287ebd93fcc104a
Milan Gruner <milangruner@gmail.com>
2023-06-26 17:19:42 +0200

    feat(test): add other entities to coverage route tests
    Signed-off-by: Milan Gruner <milangruner@gmail.com>

--- api/routes/coverage.routes.test.ts
+++ api/routes/coverage.routes.test.ts
@@ -6,6 +6,11 @@ import request from "supertest";
 import { Actor } from "../orm/actor";
 import { Publisher } from "../orm/publisher";
 import { DataSource } from "../orm/datasource";
+import { Target } from "../orm/target";
+import { EmissionsAgg } from "../orm/emissionsagg";
+import { GDP } from "../orm/gdp";
+import { Population } from "../orm/population";
+import { Territory } from "../orm/territory";
 
 const disconnect = require("../orm/init").disconnect;
 
@@ -31,6 +36,13 @@ const countryProps = {
   datasource_id: datasourceProps.datasource_id,
 };
 
+const country2Props = {
+  actor_id: "coverage_routes_test_country_2",
+  type: "country",
+  name: "Test Country 2",
+  datasource_id: datasourceProps.datasource_id,
+};
+
 const cityProps = {
   actor_id: "coverage_routes_test_city_1",
   type: "city",
@@ -38,7 +50,14 @@ const cityProps = {
   datasource_id: datasourceProps.datasource_id,
 };
 
-const region1Props = {
+const city2Props = {
+  actor_id: "coverage_routes_test_city_2",
+  type: "city",
+  name: "Test City 2",
+  datasource_id: datasourceProps.datasource_id,
+};
+
+const regionProps = {
   actor_id: "coverage_routes_test_region_1",
   type: "adm1",
   name: "Test Region",
@@ -59,9 +78,106 @@ const companyProps = {
   datasource_id: datasourceProps.datasource_id,
 };
 
-const actors = [countryProps, cityProps, region1Props, region2Props, companyProps];
+const facilityProps = {
+  actor_id: "coverage_routes_test_facility_1",
+  type: "site",
+  name: "Test Facility",
+  datasource_id: datasourceProps.datasource_id,
+};
+
+const organizationProps = {
+  actor_id: "coverage_routes_test_region_2",
+  type: "adm2",
+  name: "Test Sub Region",
+  datasource_id: datasourceProps.datasource_id,
+}
+
+const actors = [
+  countryProps,
+  country2Props,
+  cityProps,
+  city2Props,
+  regionProps,
+  region2Props,
+  companyProps,
+  facilityProps,
+  organizationProps,
+];
+
+let targetCounter = 0;
+function makeTarget(actor_id: string) {
+  targetCounter++;
+  return {
+    target_id: "coverage.routes.test.ts:target:" + targetCounter,
+    actor_id,
+    target_type: "absolute",
+    baseline_year: 2015,
+    baseline_value: 100000000,
+    target_year: 2025,
+    target_value: 50000000,
+    datasource_id: datasourceProps.datasource_id,
+    URL: "https://fake.example/countrytarget1",
+    summary: "#1 target by a country to make some changes",
+  };
+}
+
+const targets = [
+  makeTarget(countryProps.actor_id),
+  makeTarget(cityProps.actor_id),
+  makeTarget(regionProps.actor_id),
+];
+
+const emissions = [
+  {
+    emissions_id: "coverage.routes.test.ts:emissions:1",
+    actor_id: countryProps.actor_id,
+    year: 2019,
+    total_emissions: 100000,
+    datasource_id: datasourceProps.datasource_id,
+  }, {
+    emissions_id: "coverage.routes.test.ts:emissions:2",
+    actor_id: cityProps.actor_id,
+    year: 2019,
+    total_emissions: 100000,
+    datasource_id: datasourceProps.datasource_id,
+  }, {
+    emissions_id: "coverage.routes.test.ts:emissions:3",
+    actor_id: regionProps.actor_id,
+    year: 2019,
+    total_emissions: 100000,
+    datasource_id: datasourceProps.datasource_id,
+  },
+];
+
+const populationProps = {
+  actor_id: countryProps.actor_id,
+  year: 1984,
+  population: 10000000,
+  datasource_id: datasourceProps.datasource_id,
+};
+
+const gdpProps = {
+  actor_id: countryProps.actor_id,
+  year: 1984,
+  gdp: 1000000000,
+  datasource_id: datasourceProps.datasource_id,
+};
+
+const territoryProps = {
+  actor_id: countryProps.actor_id,
+  area: 9984670,
+  lat: 624000,
+  lng: -964667,
+  name: "Fake territory from coverage.routes.test.ts",
+  datasource_id: datasourceProps.datasource_id,
+};
 
 async function cleanup() {
+  await Target.destroy({ where: { datasource_id: datasourceProps.datasource_id } });
+  await EmissionsAgg.destroy({ where: { datasource_id: datasourceProps.datasource_id } });
+  await Population.destroy({ where: { datasource_id: datasourceProps.datasource_id } });
+  await GDP.destroy({ where: { datasource_id: datasourceProps.datasource_id } });
+  await Territory.destroy({ where: { datasource_id: datasourceProps.datasource_id } });
   await Actor.destroy({
     where: { datasource_id: datasourceProps.datasource_id },
   });
@@ -75,14 +191,17 @@ beforeAll(async () => {
   await cleanup();
   await Publisher.create(publisherProps);
   await DataSource.create(datasourceProps);
-  for (const props of actors) {
-    await Actor.create(props);
-  }
+  await Actor.bulkCreate(actors);
+  await Target.bulkCreate(targets);
+  await EmissionsAgg.bulkCreate(emissions);
+  await Population.create(populationProps);
+  await GDP.create(gdpProps);
+  await Territory.create(territoryProps);
 });
 
 afterAll(async () => {
   await cleanup();
-  await disconnect(); 
+  await disconnect();
 });
 
 it("returns overall coverage statistics for the data set", async () => {
@@ -104,7 +223,7 @@ it("returns overall coverage statistics for the data set", async () => {
         "number_of_facilities": 1,
         "number_of_emissions_records": 3,
         "number_of_target_records": 3,
-        "number_of_contextual_records": 3,
+        "number_of_contextual_records": 4,
         "number_of_countries_with_emissions": 1,
         "number_of_countries_with_targets": 1,
         "number_of_regions_with_emissions": 1,
